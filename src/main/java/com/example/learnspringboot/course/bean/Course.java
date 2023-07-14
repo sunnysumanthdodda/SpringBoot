554ecd88dd7cea10a718626adaf278a2619c1837
Sunny Sumanth Dodda <sunnysumanthdodda@gmail.com>
2023-07-14 06:00:50 -0500

    Spring Boot

---
+++ src/main/java/com/example/learnspringboot/course/bean/Course.java
@@ -0,0 +1,34 @@
+package com.example.learnspringboot.course.bean;
+
+public class Course {
+    private int courseId;
+    private String courseName;
+    private String courseAuthor;
+
+    public Course(int courseId, String courseName, String courseAuthor) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseAuthor = courseAuthor;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseAuthor() {
+        return courseAuthor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", courseAuthor='" + courseAuthor + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/learnspringboot/course/controller/CourseController.java
@@ -0,0 +1,20 @@
+package com.example.learnspringboot.course.controller;
+
+import com.example.learnspringboot.course.bean.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @GetMapping("/courses")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn Microservices","James Gosling"),
+                new Course(2,"Learn Full Stack","Udemy"),
+                new Course(3,"Learn JavaScript and TypeScript","Google and Meta")
+        );
+    }
+}
