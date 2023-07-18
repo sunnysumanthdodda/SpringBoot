0816a19c572c6692392df0eabc54855b45c7f0cb
Sunny Sumanth Dodda <99216785+sunnysumanthdodda@users.noreply.github.com>
2023-07-18 06:28:11 -0500

    Delete src directory

--- src/main/java/com/example/springboot/course/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.course.repository;
-
-import com.example.springboot.course.bean.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course,Integer> {
-
-}
